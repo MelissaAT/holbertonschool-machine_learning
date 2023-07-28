0f521aa2b32bc1592f74de98d2865af84b890ccf
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-28 14:38:28 -0400

    task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Documentation"""
+
+
+
+def add_matrices2D(mat1, mat2):
+    """adds two matrices element-wise"""
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[i])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+    return result
