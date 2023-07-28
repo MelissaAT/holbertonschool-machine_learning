47297c851e348108813374c6543628e6f2479a52
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-28 10:24:51 -0400

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+"""Documentation"""
+
 def matrix_shape(matrix):
+    """Shape of a matrix"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
-
