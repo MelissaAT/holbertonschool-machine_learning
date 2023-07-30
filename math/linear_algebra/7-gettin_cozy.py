b90e97ed0c1d5407bba423fc1792c14f09935079
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-30 10:14:31 -0400

    task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """Documentation"""
-import numpy as np
-
 def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
-        new_matrix = np.concatenate((mat1, mat2), axis=0)
+        new_matrix = mat1 + mat2
     elif axis == 1:
         if len(mat1) != len(mat2):
             return None
-        new_matrix = np.concatenate((mat1, mat2), axis=1)
+        new_matrix = []
+    for row1, row2 in zip(mat1, mat2):
+        new_row = row1 + row2
+        new_matrix.append(new_row)
 
-    return new_matrix.tolist()
+   
+
+    return new_matrix
+
