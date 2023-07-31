2640be01b1b3a7f61bb1334c4f36f03cbfaf211b
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-30 21:01:53 -0400

    task 13

---
+++ math/linear_algebra/13-cats_got_your-tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Documentation"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    concatenated_matrix = np.concatenate((mat1, mat2), axis=axis)
+    return concatenated_matrix
