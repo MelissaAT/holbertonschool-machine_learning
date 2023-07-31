26fe997db0febc77efde6a9a50b8e61f8b61bf00
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-30 20:19:47 -0400

    task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pyhton3
+"""Documentation"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """calculates the shape of a numpy.ndarray"""
+    shapes = matrix.shape
+    return shapes
