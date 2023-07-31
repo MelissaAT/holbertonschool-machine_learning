fcbccdc2ccfe7ed0dea91b8f0bd5af0273a6fd1f
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-07-30 21:10:16 -0400

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Documentation"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """performs matrix multiplication"""
+    result = np.dot(mat1, mat2)
+    return result
